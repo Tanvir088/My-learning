04e5019526346221224ff4fcf4439090c18b7436
“Tanvir088” <tsourov809@gmail.com>
2023-02-09 20:21:56 +0600

    Bangladesh

--- golang.go
+++ golang.go
@@ -1,12 +1,4 @@
-package main
-import"fmt"
-func main(){
-fmt.Println("Bangladesh is my mother_land.I love my mother very much *_* ")
-}
-package main
-import"fmt"
-func main(){
-fmt.Println("Bangladesh is my mother_land.I love my mother very much *_* ")
+
 
 
 package main
