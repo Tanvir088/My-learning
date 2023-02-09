a01221600339d8b064283ba232544c249eedd1f6
“Tanvir088” <tsourov809@gmail.com>
2023-02-09 19:22:54 +0600

    Natural Beauty

--- golang.go
+++ golang.go
@@ -7,3 +7,10 @@ package main
 import"fmt"
 func main(){
 fmt.Println("Bangladesh is my mother_land.I love my mother very much *_* ")
+
+
+package main
+import "fmt"
+func main(){
+fmt.println("Bangladesh is a beautyfull country and full of natural beauties")
+}
