ec849b7a025b4383ec9ee3ef652d04d09f3a92a3
“Tanvir088” <tsourov809@gmail.com>
2023-02-09 20:11:48 +0600

    Bangla Ma

--- golang.go
+++ golang.go
@@ -13,4 +13,18 @@ package main
 import "fmt"
 func main(){
 fmt.println("আমার সোনার বাংলা আমি তোমায় ভালোবাসি")
+
+}
+
+
+package main 
+import "fmt"
+func main(){
+fmt.println("সার্থক জনম আমার
+
+          জন্মেছি এই দেশে, 
+
+সার্থক জনম মা গো, 
+
+            তোমায় ভালবেসে। ")
 }
